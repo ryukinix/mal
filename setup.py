9fa24f2503ebf31ee2df80dda5156331a3bd21be
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-15 17:00:38 -0300

    Fix the problem with uninstalling package, zip_safe=False | no crash
    For some reason, this fix that problem. zip_safe is a argument whose
    controls the packaging the project with zip or not during the installation
    . The curious part is: that fucking shit is installing yet as egg.
    
    Why this doesn't install simple as a dir? Sucks

--- setup.py
+++ setup.py
@@ -6,27 +6,33 @@
 #
 #
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = '0.1'
 
 setup(
     name='mal',
     version=version,
-    description="A curatted command line interface to your personal MyAnimeList profile",
-    long_description="""Personal use for managing watching animes using MyAnimeList for
-                        people whose loves doing things on terminal and hate GUIs""",
+    description="A command line interface to your MyAnimeList profile",
+    long_description=(
+        "Personal use for managing watching animes using MyAnimeList."
+        "Aimed for people whose loves doing things on terminal and hate GUIs"
+    ),
     classifiers=[
+        "Topic :: Internet",
         "Development Status :: 3 - ALPHA",
         "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Build Tools",
     ],
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='mal myanimelist cli personal',
     author='Manoel Vilela',
+    zip_safe=False,
     author_email='manoel_vilela@engineer.com',
     url='https://github.com/ryukinix/mal',
     license='GPL',
-    packages=['mal'],
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'docs']),
+    package_dir={'build': 'mal'},
     platforms='any',
     install_requires=[
         x.strip() for x in open('requirements.txt').readlines()
