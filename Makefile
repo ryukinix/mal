ea7d8afa83d4d22fd9e5a636d727906bd2524b5b
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-15 23:48:39 -0300

    PyPI prepare: fixes about setup.py and add full-featured makefile script
    The setup.py now requires pypandoc for execute properly and send the
    long_description as reStructuredText on PyPI. If the users don't have,
    continues the build, but show a warning about this.
    
    I refactored the general classifiers to a more semantic way too.
    
    The makefile now can handle that entries:
    
    * make install # install default
    * make develop # develop mode with symlinks
    * make test-register # register in testpypi server
    * make test-deploy # deply to the testpypi server
    * make register # register in pypi server
    * make deploy # deploy release in pypi server
    * make check # check the package about errors (metadata, readme)
    * make clean # clean build trashs

--- Makefile
+++ Makefile
@@ -2,24 +2,61 @@ PYTHON = python3
 INSTALL = install
 DEVELOP = develop
 TARGET = setup.py
+TEST_DEPLOY = sdist upload --repository pypitest
+REGISTER = register --repository pypitest
+DEPLOY = sdist upload --repository pypi
+REGISTER = register --repository pypi
 TRASH = build/ dist/ *.egg-info
+CHECK = check --metadata --restructuredtext --strict
 
 all: install
+	 clean
 
 install:
-	@echo "============="
-	@echo "|  INSTALL  |"
-	@echo "============="
+	@echo "+===============+"
+	@echo "|    INSTALL    |"
+	@echo "+===============+"
 	$(PYTHON) $(TARGET) $(INSTALL)
 
 develop:
-	@echo "============="
-	@echo "|  DEVELOP  |"
-	@echo "============="
+	@echo "+===============+"
+	@echo "|    DEVELOP    |"
+	@echo "+===============+"
 	$(PYTHON) $(TARGET) $(DEVELOP)
 
+test-register:
+	@echo "+===============+"
+	@echo "| TEST-REGISTER |"
+	@echo "+===============+"
+	$(PYTHON) $(TARGET) $(REGISTER) 
+
+test-deploy:
+	@echo "+===============+"
+	@echo "| TEST-DEPLOY   |"
+	@echo "+===============+"
+	$(PYTHON) $(TARGET) $(TEST_DEPLOY) 
+
+deploy:
+	@echo "+===============+"
+	@echo "|   DEPLOY      |"
+	@echo "+===============+"
+	$(PYTHON) $(TARGET) $(DEPLOY)
+
+check:
+	@echo "+===============+"
+	@echo "|     CHECK     |"
+	@echo "+===============+"
+	$(PYTHON) $(TARGET) $(CHECK)
+	@echo "ok!"
+
+register:
+	@echo "+===============+"
+	@echo "|   REGISTER    |"
+	@echo "+===============+"
+	$(PYTHON) $(TARGET) $(REGISTER)
+
 clean:
-	@echo "============="
-	@echo "|CLEAN BUILD|"
-	@echo "============="
+	@echo "+===============+"
+	@echo "|  CLEAN BUILD  |"
+	@echo "+===============+"
 	rm -rf -v $(TRASH)
--- setup.py
+++ setup.py
@@ -7,32 +7,56 @@
 #
 
 from setuptools import setup, find_packages
+from codecs import open  # To use a consistent encoding
+from os import path
+from warnings import warn
 import mal
+
+try:
+    import pypandoc
+except ImportError:
+    warn("Only-for-developers: you need pypandoc for upload "
+         "correct reStructuredText into PyPI home page")
+
+here = path.abspath(path.dirname(__file__))
+readme = path.join(here, 'README.md')
+
+if 'pypandoc' in globals():
+    long_description = pypandoc.convert(readme, 'rst', format='markdown')
+else:
+    # Get the long description from the relevant file
+    with open(readme, encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='mal',
     version=mal.__version__,
     description="A command line interface to your MyAnimeList profile",
-    long_description=(
-        "Personal use for managing watching animes using MyAnimeList."
-        "Aimed for people whose loves doing things on terminal and hate GUIs"
-    ),
+    long_description=long_description,
     classifiers=[
+        "Environment :: Console",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
         "Topic :: Internet",
-        "Development Status :: 3 - ALPHA",
-        "Programming Language :: Python :: 3.5",
-        "Topic :: Software Development :: Build Tools",
+        "Topic :: Games/Entertainment",
+        "Topic :: Utilities",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
     # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='mal myanimelist cli personal',
+    keywords='mal myanimelist cli anime manager',
     author=mal.__author__,
     author_email=mal.__email__,
     url=mal.__url__,
+    download_url="{u}/archive/v{v}.tar.gz".format(u=mal.__url__,
+                                                  v=mal.__version__),
     zip_safe=False,
     license='GPL',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'docs']),
-    package_dir={'build': 'mal'},
-    platforms='any',
+    packages=find_packages(exclude=['ez_setup', 'examples', 
+                                    'tests', 'docs', '__pycache__']),
+    include_package_data=True,
+    platforms='unix',
     install_requires=[
         x.strip() for x in open('requirements.txt').readlines()
     ],
