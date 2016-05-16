5bf08b6a95dc8a4074c986ced1476c950bbe7646
Manoel Vilela <manoel_vilela@engineer.com>
2016-05-16 16:29:33 -0300

    Linting fixes and remove get_status_name method
    This is only used one time and, by the way, is only a wrapper to
    status_names dictionary. I do that before for some reason, try
    using this methods in map/filter functions, but now is useless

--- mal/api.py
+++ mal/api.py
@@ -14,7 +14,9 @@ from xml.etree import cElementTree as ET
 
 class MyAnimeList(object):
     base_url = 'http://myanimelist.net/api'
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36'
+    user_agent = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/34.0.1847.116 Safari/537.36')
 
     status_names = {
         1: 'watching',
@@ -22,17 +24,14 @@ class MyAnimeList(object):
         3: 'on hold',
         4: 'dropped',
         6: 'plan to watch',  # not a typo
-        7: 'rewatching' # we have rewatching here too, label for completed
-    }
+        7: 'rewatching'  # this not exists in API
+    }                    # check line
 
     status_codes = {v: k for k, v in status_names.items()}
 
     def __init__(self, config):
         self.username = config['username']
         self.password = config['password']
-
-    def get_status_name(self, status_code):
-        return self.status_names[status_code]
 
     def search(self, query):
         payload = {'q': query}
@@ -77,8 +76,8 @@ class MyAnimeList(object):
                     'status': int(entry['my_status']),
                     'score': int(entry['my_score']),
                     'total_episodes': int(entry['series_episodes']),
-                    'rewatching': int(entry['my_rewatching']) if entry['my_rewatching'] else 0,
-                    'status_name': self.get_status_name(int(entry['my_status'])),
+                    'rewatching': int(entry['my_rewatching'] or 0),
+                    'status_name': self.status_names[int(entry['my_status'])],
                 }
                 # if was rewatching, so the status_name is rewatching
                 if result[entry_id]['rewatching']:
