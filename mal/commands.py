08437728b42b2f5e12a272a04dc55135913b4b7e
Miguel Paradinha <miguel.p.paradinha@gmail.com>
2017-01-25 23:13:54 +0000

    more work on search printing

--- mal/commands.py
+++ mal/commands.py
@@ -22,7 +22,7 @@ from mal import setup
 
 def search(mal, args):
     """Search MAL (not just the user) anime database."""
-    core.search(mal, args.anime_regex.lower())
+    core.search(mal, args.anime_regex.lower(), args.full)
 
 
 def filter(mal, args):
