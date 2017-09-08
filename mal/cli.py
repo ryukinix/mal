3e5a150e4f1693fcbd7176826aebee38a7a60454
Manoel Vilela <manoel_vilela@engineer.com>
2017-09-08 20:26:18 -0300

    Add version -v --version flag to mal
    This can be useful to users check which version of mal they got
    installed.

--- mal/cli.py
+++ mal/cli.py
@@ -13,6 +13,7 @@ import signal
 import argparse
 
 # self-package
+import mal
 from mal.api import MyAnimeList
 from mal.utils import killed
 from mal import color
@@ -26,6 +27,10 @@ signal.signal(signal.SIGINT, lambda x, y: killed())
 def create_parser():
     parser = argparse.ArgumentParser(prog='mal',
                                      description='MyAnimeList command line client.')
+    parser.add_argument('-v',
+                        '--version',
+                        action='store_true',
+                        help='show the version of mal')
     subparsers = parser.add_subparsers(help='commands', dest='command')
 
     # Parser for "search" command
@@ -147,6 +152,10 @@ def main():
     else:
         args = parser.parse_args()
 
+    if args.version:
+        print(mal.__version__)
+        sys.exit(0)
+
     # if the command is login, create credentials and exits
     # NOTE: if this statement is removed the `mal login` and
     # no credentials exists, login.create_credentials() will
@@ -157,14 +166,14 @@ def main():
 
     # Check if authorized
     config = login.get_credentials()
-    mal = MyAnimeList.login(config)
-    if not mal:
+    mal_api = MyAnimeList.login(config)
+    if not mal_api:
         print(color.colorize('Invalid credentials! :(', 'red', 'bold'))
         print(color.colorize('Tip: Try "mal login" again :D', 'white', 'bold'))
         sys.exit(1)
 
     # Execute sub command
-    args.func(mal, args)
+    args.func(mal_api, args)
 
 
 if __name__ == '__main__':
