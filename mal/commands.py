3979293467ccb54e1f3273c3c9aeabf5936ae286
Manoel Vilela <manoel_vilela@engineer.com>
2018-03-17 22:46:14 -0300

    [cli] Update mal.commands.search anime_regex -> anime_title

--- mal/commands.py
+++ mal/commands.py
@@ -22,7 +22,7 @@ from mal import setup
 
 def search(mal, args):
     """Search MAL (not just the user) anime database."""
-    core.search(mal, args.anime_regex.lower(), full=args.extend)
+    core.search(mal, args.anime_title.lower(), full=args.extend)
 
 
 def filter(mal, args):
